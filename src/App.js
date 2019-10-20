ff95d59b910701abc18f0d040de69f40401ba515
bigtonito39 <bigtonito@hotmail.com>
2019-10-20 18:25:42 -0400

    Almost all stretch done, just missing 1 stretch

--- src/App.js
+++ src/App.js
@@ -4,78 +4,61 @@ import "./App.css";
 import BottomRow from "./BottomRow";
 import ReactDOM from "react-dom";
 
-function App() {
+
+ 
+
+export function App() {
     //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
     const [homeCount, sethomeCount] = useState(0);
     const [awayCount, setawayCount] = useState(0);
-
-
+    
+    
     const touchDownHOME = () => sethomeCount(homeCount + 4);
     const touchDownAWAY = () => setawayCount(awayCount + 4);
-
+    
     const fieldGoalHOME = () => sethomeCount(homeCount + 1);
     const fieldGoalAWAY = () => setawayCount(awayCount + 1);
 
 
-
-
-    return ( <
-        div className = "container" >
-        <
-        section className = "scoreboard" >
-        <
-        div className = "topRow" >
-        <
-        div className = "home" >
-        <
-        h2 className = "home__name" > Mets < /h2>
+    return ( <div className = "container" >
+        <section className = "scoreboard" >
+        <div className = "topRow" >
+        <div className = "home" >
+        <h2 className = "home__name" > Mets </h2>
 
         { /* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */ }
 
-        <
-        div className = "home__score" > { homeCount } < /div> < /
-        div > <
-        div className = "timer" > "LeTs Go METS!" < /div> <
-        div className = "away" >
-        <
-        h2 className = "away__name" > Phillies < /h2> <
-        div className = "away__score" > { awayCount } < /div> < /
-        div > <
-        /div> <
+        <div className = "home__score" > { homeCount } </div> 
+        
+        </div >
+        <div className = "timer" > "LeTs Go METS!" </div>  
+        <div className = "away" >
+        <h2 className = "away__name" > Phillies </h2>  
+        <div className = "away__score" > { awayCount } </div>  
+        </div > 
+        </div> 
+        
+        <BottomRow  />
+        </section> 
 
-        BottomRow
-
-
-        /
-        >
-
-
-        <
-        /section> <
-        section className = "buttons" >
-        <
-        div className = "homeButtons" > { /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */ } <
-        button className = "homeButtons__touchdown"
-        onClick = { touchDownHOME } > Home Grand Slam! < /button> <
-        button className = "homeButtons__fieldGoal"
-        onClick = { fieldGoalHOME } > Home Home Run! < /button> < /
-        div > <
-        div className = "awayButtons" >
-        <
-        button className = "awayButtons__touchdown"
-        onClick = { touchDownAWAY } > Away Grand Slam! < /button> <
-        button className = "awayButtons__fieldGoal"
-        onClick = { fieldGoalAWAY } > Away Home Run! < /button> < /
-        div >
-
-
-
-        <
-        /section> </div >
+        <section className = "buttons" >
+        <div className = "homeButtons"> { /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */ } 
+        <button className = "homeButtons__touchdown" onClick = { touchDownHOME } > Home Grand Slam! </button>  
+        <button className = "homeButtons__fieldGoal" onClick = { fieldGoalHOME } > Home Home Run! </button>  
+        
+        </div> 
+        <div className = "awayButtons" >
+        <button className = "awayButtons__touchdown" onClick = { touchDownAWAY } > Away Grand Slam! </button>  
+        <button className = "awayButtons__fieldGoal" onClick = { fieldGoalAWAY } > Away Home Run! </button>  
+        
+        </div > 
+        </section> </div >
     );
 }
+
+
 
 export default App;
 
 const rootElement = document.getElementById("root");
-ReactDOM.render( < App / > , rootElement);
+ReactDOM.render( < App/> , rootElement);
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
+
+
 const BottomRow = () => {
+
+const [hitHome,sethitHome] = useState(0);
+const [hitAway,sethitAway] = useState(0);
+const [errorHome,seterrorHome] = useState(0);
+const [errorAway,seterrorAway] = useState(0);
+
+const hitForHomeTeam = () => sethitHome(hitHome + 1);
+const hitForAwayTeam = () => sethitAway(hitAway + 1);
+const errorForHomeTeam = () => seterrorHome(errorHome + 1);
+const errorForAwayTeam = () => seterrorAway(errorAway + 1);
+
+
+
+
+function HitChanger (props) {
+   const {gothit, teamName} = props;
+    return <button onClick = {gothit} >{teamName}</button>
+  
+  }
+
+  function ErrorChanger (props) {
+   const {gotError, teamName} = props;
+   return <button onClick = {gotError}>{teamName}</button>
+
+  }
+
+
   return (
     <div className="bottomRow">
       <div className="down">
-        <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
-      </div>
+        <h3 className="down__title">Home Hits</h3>
+        <div className="down__value">{hitHome}</div>
+    <HitChanger gothit = {hitForHomeTeam} teamName = "Mets" />
+       </div>
       <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
-      </div>
+        <h3 className="toGo__title">Home Errors</h3>
+        <div className="toGo__value">{errorHome}</div>
+       <ErrorChanger gotError = {errorForHomeTeam} teamName ="Mets" /> 
+        </div>
       <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <h3 className="ballOn__title">Guest Hits</h3>
+        <div className="ballOn__value">{hitAway}</div>
+        <HitChanger gothit = {hitForAwayTeam} teamName = "Phillies" />
       </div>
       <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <h3 className="quarter__title">Guest Errors</h3>
+        <div className="quarter__value">{errorAway}</div>
+        <ErrorChanger gotError = {errorForAwayTeam} teamName ="Phillies" /> 
       </div>
     </div>
   );
